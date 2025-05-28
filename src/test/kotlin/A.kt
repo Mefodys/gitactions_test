22de90b8c17f587d95ad58e87b1906d902605b0e
anton.mefodichev <mefody123@gmail.com>
2025-05-28 21:39:15 +0200

    added tests

---
+++ src/test/kotlin/A.kt
@@ -0,0 +1,17 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestMy {
+    @Test
+    fun foo_test(){
+
+        assertEquals(1,1)
+    }
+    @Test
+    fun foo_test2(){
+
+        assertEquals(1,2)
+    }
+
+}
+
